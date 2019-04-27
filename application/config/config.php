b0edee0f4f66de041f6d09885b7c5b845368cab4
Danar Widi <widiu7omo@gmail.com>
2019-04-27 13:01:32 +0800

    remove index.php

--- application/config/config.php
+++ application/config/config.php
@@ -37,7 +37,7 @@ $config['base_url'] = 'https://simprakerin.herokuapp.com/';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
